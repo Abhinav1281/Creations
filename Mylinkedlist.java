d7d5efb07ecb404738c6bcdc05bf3330d27a9c38
Abhinav1281 <abhisione2@gmail.com>
2020-07-09 18:51:31 +0530

    New commit to the linkedlist creation with multiple functions

--- Mylinkedlist.java
+++ Mylinkedlist.java
@@ -1,10 +1,10 @@
 package Creations;
 
-public class Mylinkedlist {
+public class Mylinkedlist<A> {
 		
 		Node head;
 		
-		public void add(int pos,int data)
+		public void add(int pos,A data)
 		{
 			Node toadd = new Node(data);
 			if(isEmpty())
@@ -14,17 +14,20 @@ public class Mylinkedlist {
 			else 	
 			{
 				Node temp = head;
-				temp=head;
-				for(int i=0;i<pos;i++)
+				Node temp1=temp.prev;
+				for(int i=0;i<=pos;i++)
 				{
+					temp1=temp;
 					temp=temp.next;
 				}
-				temp.next=toadd;
+				toadd.next=temp;
+				toadd.prev=temp1;
+				temp1.next=toadd;
 		
 			}
 		}
 		
-		public void add(int data)
+		public void add(A data)
 		{
 			Node toadd = new Node(data);
 			if(isEmpty())
@@ -41,10 +44,11 @@ public class Mylinkedlist {
 				temp.next=toadd;
 			}	
 		}
-		
+	
 		public void print()
 		{
 			Node temp = head;
+			System.out.println();
 			while(temp!=null)
 			{
 				System.out.print(temp.data+" ");
@@ -52,19 +56,99 @@ public class Mylinkedlist {
 			}
 		}
 		
+		public int size()
+		{
+			Node temp=head;
+			int s=0;
+			while(temp!=null)
+			{
+				temp=temp.next;
+				s++;
+			}
+			return s;
+		}
 		public boolean isEmpty()
 		{
 			return (head==null);
 		}
 		
-		static class Node
+		public A get(int pos)
 		{
-			int data;
+			Node temp=head;
+			for(int i=0;i<pos;i++)
+			{
+				temp=temp.next;
+			}
+			return temp.data;
+		}
+		
+		public void set(int pos,A val)
+		{
+			Node temp=head;
+			for(int i=0;i<pos;i++)
+			{
+				temp=temp.next;
+			}
+			temp.data=val;
+		}
+		
+		public void remove(int pos)
+		{
+			Node temp=head;
+			Node temp1=temp.prev;
+			for(int i=0;i<pos;i++)
+			{
+				temp1=temp;
+				temp=temp.next;
+			}
+			temp1.next=temp.next;
+		}
+		
+		public void removeall(A val)
+		{
+			Node temp=head;
+			Node temp1=temp.prev;
+			while(temp!=null)
+			{
+				if(temp.data.equals(val))
+				{
+					temp1.next=temp.next;
+					temp=temp1.next;
+					continue;
+				}
+				else
+				{
+					temp1=temp;
+					temp=temp.next;
+				}
+			}
+		}
+		public void clear()
+		{
+			head.next=null;
+			head.prev=null;		
+		}
+		public boolean contains(A val)
+		{
+			Node temp=head;
+			while(temp!=null)
+			{
+				if(temp.data.equals(val))
+					return true;
+				temp=temp.next;
+			}
+			return false;
+		}
+		class Node
+		{
+			A data;
 			Node next;
+			Node prev;
 			
-			Node(int data){
+			Node(A data){
 				this.data=data;
 				this.next=null;
+				this.prev=null;
 			}
 		}
 
